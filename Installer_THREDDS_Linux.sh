d736a61b6844d0c72a50430e87ab7e808af93eb0
Nabil M <nmiri127@gmail.com>
2018-05-04 11:30:02 -0230

    Finally got this nonsense working.

--- Installer_THREDDS_Linux.sh
+++ Installer_THREDDS_Linux.sh
@@ -18,25 +18,18 @@ sudo echo oracle-java10-installer shared/accepted-oracle-license-v1-1 select tru
 sudo apt -y install oracle-java10-installer
 sudo apt -y install oracle-java10-set-default
 
-echo javac -version
-
 echo
-echo "Installing Tomcat 9..."
-# https://askubuntu.com/questions/777342/how-to-install-tomcat-9
-# https://www.rosehosting.com/blog/install-tomcat-9-on-an-ubuntu-16-04-vps/
+echo "Acquiring Tomcat 9 + THREDDS..."
 sudo useradd -r tomcat --shell /bin/false
 cd /opt
-sudo wget http://archive.apache.org/dist/tomcat/tomcat-9/v9.0.7/bin/apache-tomcat-9.0.7.tar.gz
-sudo tar -xzf apache-tomcat-9.0.7.tar.gz
-sudo rm apache-tomcat-9.0.7.tar.gz
-sudo mv apache-tomcat-9.0.7 tomcat9
-sudo chown -hR tomcat: tomcat9
+if [ -d "/opt/tomcat9/" ]: then
+    sudo rm -r /opt/tomcat9
+fi
+sudo wget http://navigator.oceansdata.ca/cdn/tomcat9-thredds.tar.gz
+sudo tar -xjC /opt/ -f tomcat9-thredds.tar.gz
 
-echo
-echo "Installing THREDDS..."
-sudo wget ftp://ftp.unidata.ucar.edu/pub/thredds/4.6/current/thredds.war -P /opt/tomcat9/webapps/
-sudo chown tomcat /opt/tomcat9/webapps/thredds.war
-sudo chgrp tomcat /opt/tomcat9/webapps/thredds.war
+sudo chown -R tomcat /opt/tomcat9/
+sudo chgrp -R tomcat /opt/tomcat9/
 
 sudo mkdir /usr/local/tomcat
 sudo mkdir /usr/local/tomcat/content
@@ -44,41 +37,9 @@ sudo mkdir /usr/local/tomcat/content
 sudo chown -R tomcat /usr/local/tomcat
 sudo chgrp -R tomcat /usr/local/tomcat
 
-echo "#!/bin/sh
+echo
+echo "Starting server..."
+sudo bash /opt/tomcat9/bin/startup.sh
 
-#
-# ENVARS for Tomcat
-#
-export CATALINA_HOME="/opt/tomcat9"
-export CATALINA_BASE="/opt/tomcat9"
-export JAVA_HOME="/usr/lib/jvm/java-10-oracle"
-export JRE_HOME="/usr/lib/jvm/java-10-oracle"
-
-
-# TDS specific ENVARS
-#
-# Define where the TDS content directory will live
-#   THIS IS CRITICAL and there is NO DEFAULT - the
-#   TDS will not start without this.
-#
-CONTENT_ROOT=-Dtds.content.root.path=/usr/local/tomcat/content
-
-# set java prefs related variables (used by the wms service, for example)
-JAVA_PREFS_ROOTS="-Djava.util.prefs.systemRoot=$CATALINA_HOME/content/thredds/javaUtilPrefs \
-                  -Djava.util.prefs.userRoot=$CATALINA_HOME/content/thredds/javaUtilPrefs"
-
-#
-# Some commonly used JAVA_OPTS settings:
-#
-NORMAL="-d64 -Xmx4096m -Xms512m -server -ea"
-HEAP_DUMP="-XX:+HeapDumpOnOutOfMemoryError"
-HEADLESS="-Djava.awt.headless=true"
-
-#
-# Standard setup.
-#
-JAVA_OPTS="$CONTENT_ROOT $NORMAL $MAX_PERM_GEN $HEAP_DUMP $HEADLESS $JAVA_PREFS_ROOTS"
-
-export JAVA_OPTS" > /opt/tomcat9/bin/setenv.sh
-
-echo "Setup complete! To start Tomcat + THREDDS: bash /opt/tomcat9/bin/startup.sh"
+echo
+echo "Install complete! Visit localhost:8080/thredds/"
