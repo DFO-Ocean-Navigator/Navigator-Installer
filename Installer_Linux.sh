5586a184a0b4d7b8d848ff42da87ba83597a16f2
Nabil M <nmiri127@gmail.com>
2018-08-07 08:57:55 -0230

    Merged THREDDS installer into Linux installer.

--- Installer_Linux.sh
+++ Installer_Linux.sh
@@ -58,18 +58,62 @@ echo "Setting PATH if needed..."
 [[ ":$PATH:" != *":/opt/tools/miniconda3/bin:"* ]] && echo 'export PATH=/opt/tools/miniconda3/bin/:$PATH' >> ~/.bashrc
 source ~/.bashrc
 
+echo "Enter directory where local datasets should be stored [/opt/thredds_content/]:"
+read THREDDS_CONTENT_DIR
+if [ -z "${THREDDS_CONTENT_DIR// }" ]; then # Expand user input and spaces with nothing
+    # Empty string given by user
+    THREDDS_CONTENT_DIR=/opt/thredds_content
+fi
+
 echo
-echo "Grabbing lastest version of Config Tool..."
-curl -s https://api.github.com/repos/DFO-Ocean-Navigator/Ocean-Navigator-Config-Tool/releases/latest \
+echo "Installing Java 10..."
+sudo add-apt-repository ppa:linuxuprising/java -y
+sudo apt update
+sudo echo oracle-java10-installer shared/accepted-oracle-license-v1-1 select true | sudo /usr/bin/debconf-set-selections
+sudo apt -y install oracle-java10-installer
+sudo apt -y install oracle-java10-set-default
+
+echo
+echo "Acquiring Tomcat 9 + THREDDS..."
+if [ -d "/opt/tomcat9/" ]; then
+    sudo rm -r /opt/tomcat9
+fi
+wget http://navigator.oceansdata.ca/cdn/tomcat9-thredds.tar.gz
+tar -xjC /home/$USER/ -f tomcat9-thredds.tar.gz
+sudo mv /home/$USER/tomcat9 /opt/
+sudo chown -R $USER:$USER /opt/tomcat9/
+rm tomcat9-thredds.tar.gz
+
+if [ ! -d $THREDDS_CONTENT_DIR ]; then
+    sudo mkdir -pv $THREDDS_CONTENT_DIR
+    sudo chown -R $USER:$USER $THREDDS_CONTENT_DIR
+fi
+
+sed -i "s@TDS_CONTENT@$THREDDS_CONTENT_DIR" /opt/tomcat9/bin/setenv.sh
+
+echo
+echo "Starting server..."
+bash /opt/tomcat9/bin/startup.sh
+echo "To shutdown: bash /opt/tomcat9/bin/shutdown.sh"
+
+echo
+echo "THREDDS Install complete! Visit localhost:8080/thredds/"
+
+echo
+echo "Grabbing lastest version of Navigator2Go..."
+curl -s https://api.github.com/repos/DFO-Ocean-Navigator/Navigator2Go/releases/latest \
 | grep "browser_download_url.*tar.gz" \
 | cut -d : -f 2,3 \
 | tr -d \" \
 | wget -qi -
-tar -xvvf Config_Tool.tar.gz $install_dir/Ocean-Navigator-Config-Tool/
-rm Config_Tool.tar.gz
-echo "The config tool is found in: $install_dir/Ocean-Navigator-Config-Tool/"
+tar -xvvf Navigator2Go.tar.gz $install_dir/Navigator2Go/
+rm Navigator2Go.tar.gz
+echo "Navigator2Go executable is found in: $install_dir/Navigator2Go/"
 
 # Cleanup
 echo
 echo "Cleaning up..."
 sudo apt -y autoremove
+
+echo
+echo "All done!"
