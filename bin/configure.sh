59e4b75e2763cf6dfdb5044aa0403b0c69c38496
Dwayne Hart <dwayne.hart@dfo-mpo.gc.ca>
2020-11-04 19:11:28 -0330

    Changed the logic contained in the bin/configure.sh file.

--- bin/configure.sh
+++ bin/configure.sh
@@ -3,8 +3,10 @@
 if [ ! -e bin/.configuration.updated ] ; then
    sed "s#\/home\/buildadm\/cache\/oceannavigator#$(echo $HOME)\/cache\/oceannavigator#g" -i ../Ocean-Data-Map-Project/oceannavigator/oceannavigator.cfg
    sed "s#LOCATION#$(echo $HOME)#g" -i ../tools/conf/ocean-navigator-env.sh
+   git submodule update --init --recursive
+   cd tools/nvm
+   git checkout -b tags/v0.36.0 v0.36.0
+   cd -
    touch .configuration.updated
 fi
 
-cd tools/nvm
-git checkout tags/v0.36.0 -b v0.36.0
