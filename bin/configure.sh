5c1b6b9df2825cb8ce3b7a2ad1a0ad5a8db28018
Dwayne Hart <dwayne.hart@dfo-mpo.gc.ca>
2020-11-04 19:30:15 -0330

    Putting all the git work ahead of the configuration file.

--- bin/configure.sh
+++ bin/configure.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env bash
 
 if [ ! -e bin/.configuration.updated ] ; then
-   sed "s#\/home\/buildadm\/cache\/oceannavigator#$(echo $HOME)\/cache\/oceannavigator#g" -i Ocean-Data-Map-Project/oceannavigator/oceannavigator.cfg
-   sed "s#LOCATION#$(echo $HOME)#g" -i tools/conf/ocean-navigator-env.sh
    git submodule update --init --recursive
    cd tools/nvm
    git checkout -b tags/v0.36.0 v0.36.0
    cd -
+   sed "s#\/home\/buildadm\/cache\/oceannavigator#$(echo $HOME)\/cache\/oceannavigator#g" -i Ocean-Data-Map-Project/oceannavigator/oceannavigator.cfg
+   sed "s#LOCATION#$(echo $HOME)#g" -i tools/conf/ocean-navigator-env.sh
    touch .configuration.updated
 fi
 
