cb613ab03884bbf92031b85b4dcaaa0dceedca47
Dwayne Hart <44408505+dwayne-hart@users.noreply.github.com>
2020-11-04 20:44:33 -0330

    Updates 202011 (#24)
    * Restructuring the installation script.
    
    * Added in some changes to the bin/configure.sh file to replace certain strings in the oceannavigator/configs/oceannavigator.cfg and environment configuration files.
    
    * Adjusting the configure.sh script and created the cache directory.
    
    * Correcting the submodule file to bring in tools/{nvm,netcdf-timestamp-mapper}.
    
    * Correcting formatting in bin/configure.sh.
    
    * Changed the logic contained in the bin/configure.sh file.
    
    * Corrected the path to the Ocean-Data-Map-Project/oceannavigator/oceannavigator.cfg and tools/conf/ocean-navigator-env.sh files.
    
    * Putting all the git work ahead of the configuration file.
    
    * Corrected the location of a test file.
    
    * Update the README.md and install.sh files.
    
    Co-authored-by: Dwayne Hart <dwayne.hart@dfo-mpo.gc.ca>

--- install.sh
+++ install.sh
@@ -9,16 +9,17 @@ cd /home ; git clone https://github.com/DFO-Ocean-Navigator/Navigator-Installer.
 
 cd ${STD_USER_ACCT} ; git submodule update --init --recursive
 
-bash Miniconda3-latest-Linux-x86_64.sh -b -p /home/${STD_USER_ACCT}/miniconda/3/amd64
+[ -d /home/${STD_USER_ACCT}/tools/miniconda ] && rm -rf /home/${STD_USER_ACCT}/tools/miniconda
+
+bash Miniconda3-latest-Linux-x86_64.sh -b -p /home/${STD_USER_ACCT}/tools/miniconda/3/amd64
 
 chown -R ${STD_USER_ACCT}:${STD_USER_ACCT} /home/${STD_USER_ACCT}/ 
 
-sed -i "s/buildadm/${STD_USER_ACCT}/" /home/${STD_USER_ACCT}/.bashrc
-
-su - ${STD_USER_ACCT} -c "/home/${STD_USER_ACCT}/miniconda/3/amd64/bin/conda create --name navigator --file /home/${STD_USER_ACCT}/Ocean-Data-Map-Project/config/conda/navigator-spec-file.txt"
-su - ${STD_USER_ACCT} -c "/home/${STD_USER_ACCT}/miniconda/3/amd64/bin/conda create --name index-tool --file /home/${STD_USER_ACCT}/Ocean-Data-Map-Project/config/conda/index-tool-spec-file.txt"
-su - ${STD_USER_ACCT} -c "/home/${STD_USER_ACCT}/miniconda/3/amd64/bin/conda create --name nco-tools --file /home/${STD_USER_ACCT}/Ocean-Data-Map-Project/config/conda/nco-tools-spec-file.txt"
-su - ${STD_USER_ACCT} -c "/home/${STD_USER_ACCT}/miniconda/3/amd64/bin/conda create --name devops --file /home/${STD_USER_ACCT}/Ocean-Data-Map-Project/config/conda/devops-spec-file.txt"
+su - ${STD_USER_ACCT} -c "/home/${STD_USER_ACCT}/tools/miniconda/3/amd64/bin/conda create --name navigator --file /home/${STD_USER_ACCT}/Ocean-Data-Map-Project/config/conda/navigator-spec-file.txt"
+su - ${STD_USER_ACCT} -c "/home/${STD_USER_ACCT}/tools/miniconda/3/amd64/bin/conda create --name index-tool --file /home/${STD_USER_ACCT}/Ocean-Data-Map-Project/config/conda/index-tool-spec-file.txt"
+su - ${STD_USER_ACCT} -c "/home/${STD_USER_ACCT}/tools/miniconda/3/amd64/bin/conda create --name nco-tools --file /home/${STD_USER_ACCT}/Ocean-Data-Map-Project/config/conda/nco-tools-spec-file.txt"
+su - ${STD_USER_ACCT} -c "/home/${STD_USER_ACCT}/tools/miniconda/3/amd64/bin/conda create --name devops --file /home/${STD_USER_ACCT}/Ocean-Data-Map-Project/config/conda/devops-spec-file.txt"
+su - ${STD_USER_ACCT} -c "/home/${STD_USER_ACCT}/tools/miniconda/3/amd64/bin/conda create --name devops --file /home/${STD_USER_ACCT}/Ocean-Data-Map-Project/config/conda/devops-spec-file.txt"
 
 if [ ! -d /data ] && [ -f /home/${STD_USER_ACCT}/.ssh/id_rsa ] ; then
    mkdir /data && chown ${STD_USER_ACCT}:${STD_USER_ACCT} /data
@@ -33,8 +34,11 @@ echo ""
 echo "   Supporting software installation and environment setup has completed. Now become the ${STD_USER_ACCT} user"
 echo "   and perform the following steps:"
 echo ""
-echo "   - install node and bower"
-echo "       * nvm install v8.16.0 ; yarn global add bower"
+echo "   - run the bin/configure.sh script to alter directory locations"
+echo "       * bin/configure.sh"
+echo ""
+echo "   - source the Ocean Navigator environment file"
+echo "       * source tools/conf/ocean-navigator-env.sh"
 echo ""
 echo "   - change your current working directory to Ocean-Data-Map-Project/oceannavigator/frontend and compile"
 echo "     the frontend codebase"
