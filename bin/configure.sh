80bcf17e1e71c5953e9f14950bd145150e4379fa
Dwayne Hart <dwayne.hart@dfo-mpo.gc.ca>
2020-11-05 18:41:48 -0330

    Remove the provided version of miniconda, automate the installation, and setup the various virtual environments.

--- bin/configure.sh
+++ bin/configure.sh
@@ -1,5 +1,16 @@
 #!/usr/bin/env bash
 
+if [ ! -e bin/.miniconda.installed ] ; then
+   rm -rf tools/miniconda
+   bash Miniconda3-latest-Linux-x86_64.sh -b -p tools/miniconda/3/amd64
+   ./tools/miniconda/3/amd64/bin/conda create --name navigator --file Ocean-Data-Map-Project/config/conda/navigator-spec-file.txt
+   ./tools/miniconda/3/amd64/bin/conda create --name index-tool --file Ocean-Data-Map-Project/config/conda/index-tool-spec-file.txt
+   ./tools/miniconda/3/amd64/bin/conda create --name nco-tools --file Ocean-Data-Map-Project/config/conda/nco-tools-spec-file.txt
+   ./tools/miniconda/3/amd64/bin/conda create --name devops --file Ocean-Data-Map-Project/config/conda/devops-spec-file.txt
+   ./tools/miniconda/3/amd64/bin/conda create --name devops --file Ocean-Data-Map-Project/config/conda/devops-spec-file.txt
+   touch bin/.miniconda.installed
+fi
+
 if [ ! -e bin/.configuration.updated ] ; then
    git submodule update --init --recursive
    cd tools/nvm
