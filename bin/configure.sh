5e9689cce170671460f05c0769da79da6003f497
Dwayne Hart <dwayne.hart@dfo-mpo.gc.ca>
2020-11-05 18:52:53 -0330

    Corrected an issue with application flow.

--- bin/configure.sh
+++ bin/configure.sh
@@ -1,5 +1,15 @@
 #!/usr/bin/env bash
 
+if [ ! -e bin/.configuration.updated ] ; then
+   git submodule update --init --recursive
+   cd tools/nvm
+   git checkout -b tags/v0.36.0 v0.36.0
+   cd -
+   sed "s#\/home\/buildadm\/cache\/oceannavigator#$(echo $HOME)\/cache\/oceannavigator#g" -i Ocean-Data-Map-Project/oceannavigator/oceannavigator.cfg
+   sed "s#LOCATION#$(echo $HOME)#g" -i tools/conf/ocean-navigator-env.sh
+   touch bin/.configuration.updated
+fi
+
 if [ ! -e bin/.miniconda.installed ] ; then
    rm -rf tools/miniconda
    bash Miniconda3-latest-Linux-x86_64.sh -b -p tools/miniconda/3/amd64
@@ -9,15 +19,5 @@ if [ ! -e bin/.miniconda.installed ] ; then
    ./tools/miniconda/3/amd64/bin/conda create --name devops --file Ocean-Data-Map-Project/config/conda/devops-spec-file.txt
    ./tools/miniconda/3/amd64/bin/conda create --name devops --file Ocean-Data-Map-Project/config/conda/devops-spec-file.txt
    touch bin/.miniconda.installed
-fi
-
-if [ ! -e bin/.configuration.updated ] ; then
-   git submodule update --init --recursive
-   cd tools/nvm
-   git checkout -b tags/v0.36.0 v0.36.0
-   cd -
-   sed "s#\/home\/buildadm\/cache\/oceannavigator#$(echo $HOME)\/cache\/oceannavigator#g" -i Ocean-Data-Map-Project/oceannavigator/oceannavigator.cfg
-   sed "s#LOCATION#$(echo $HOME)#g" -i tools/conf/ocean-navigator-env.sh
-   touch bin/.configuration.updated
 fi
 
