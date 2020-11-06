a9a155c78e842a7855f66dcfc51ab6b4b938861b
Dwayne Hart <dwayne.hart@dfo-mpo.gc.ca>
2020-11-06 00:04:50 -0330

    Removed the existing version of tools/miniconda in order to allow our script to automatically install it (refer to pull request #26.

--- tools/miniconda/3/amd64/envs/navigator/lib/xml2Conf.sh
+++
@@ -1,8 +0,0 @@
-#
-# Configuration file for using the XML library in GNOME applications
-#
-XML2_LIBDIR="-L/mnt/ramdisk/tools/miniconda/3/amd64/envs/navigator/lib"
-XML2_LIBS="-lxml2 -L/mnt/ramdisk/tools/miniconda/3/amd64/envs/navigator/lib -lz -L/mnt/ramdisk/tools/miniconda/3/amd64/envs/navigator/lib -llzma -lpthread -L/mnt/ramdisk/tools/miniconda/3/amd64/envs/navigator/lib -liconv -L/mnt/ramdisk/tools/miniconda/3/amd64/envs/navigator/lib -licui18n -licuuc -licudata -lm "
-XML2_INCLUDEDIR="-I/mnt/ramdisk/tools/miniconda/3/amd64/envs/navigator/include/libxml2 -I/mnt/ramdisk/tools/miniconda/3/amd64/envs/navigator/include"
-MODULE_VERSION="xml2-2.9.9"
-
