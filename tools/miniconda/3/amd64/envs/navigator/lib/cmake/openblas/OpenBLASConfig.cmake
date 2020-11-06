224cbcf5eff77932b03100d6d86f9f72f1360e48
Dwayne Hart <44408505+dwayne-hart@users.noreply.github.com>
2020-11-06 00:07:25 -0330

    Removed the existing version of tools/miniconda in order to allow our script to automatically install it (refer to pull request #26. (#27)
    Co-authored-by: Dwayne Hart <dwayne.hart@dfo-mpo.gc.ca>

--- tools/miniconda/3/amd64/envs/navigator/lib/cmake/openblas/OpenBLASConfig.cmake
+++
@@ -1,3 +0,0 @@
-SET(OpenBLAS_VERSION "0.3.3")
-SET(OpenBLAS_INCLUDE_DIRS /mnt/ramdisk/tools/miniconda/3/amd64/envs/navigator/include)
-SET(OpenBLAS_LIBRARIES /mnt/ramdisk/tools/miniconda/3/amd64/envs/navigator/lib/libopenblas.so)
