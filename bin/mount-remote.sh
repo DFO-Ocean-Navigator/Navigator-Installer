4583eec7892674ed5f0d21d97f3dd4fe955a5ef4
Dwayne Hart <44408505+dwayne-hart@users.noreply.github.com>
2021-01-07 16:48:04 -0330

    Update mount-remote.sh
    Ensuring that when the sshfs binary is used to mount a remote file system. It will  be mounted read only.

--- bin/mount-remote.sh
+++ bin/mount-remote.sh
@@ -3,4 +3,4 @@
 ssh-keygen -R hank.research.cs.dal.ca -f ${HOME}/.ssh/known_hosts
 ssh-keyscan -4 -t rsa hank.research.cs.dal.ca -f ${HOME}/.ssh/known_hosts
 
-sshfs -o reconnect,ServerAliveInterval=15,ServerAliveCountMax=3 on-buildadm@hank.research.cs.dal.ca:/tank/data /data
+sshfs -o ro,reconnect,ServerAliveInterval=15,ServerAliveCountMax=3 on-buildadm@hank.research.cs.dal.ca:/tank/data /data
