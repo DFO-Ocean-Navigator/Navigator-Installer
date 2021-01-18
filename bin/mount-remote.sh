17a287b2c4db9341d19d870ffdba77cc15b63b85
Dwayne Hart <44408505+dwayne-hart@users.noreply.github.com>
2021-01-18 11:37:09 -0330

    Update mount-remote.sh
    Change the order of the options used by ssh-keyscan.
    
    Note that in order to be able to use the allow_other option in the sshfs command. The Linux /etc/fuse.conf file will need to have the line containing the string "user_allow_other" uncommented.

--- bin/mount-remote.sh
+++ bin/mount-remote.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
 ssh-keygen -R hank.research.cs.dal.ca -f ${HOME}/.ssh/known_hosts
-ssh-keyscan -4 -t rsa hank.research.cs.dal.ca -f ${HOME}/.ssh/known_hosts
+ssh-keyscan -4 -f ${HOME}/.ssh/known_hosts -t rsa hank.research.cs.dal.ca
 
-sshfs -o ro,reconnect,ServerAliveInterval=15,ServerAliveCountMax=3 on-buildadm@hank.research.cs.dal.ca:/tank/data /data
+sshfs -o ro,reconnect,allow_other,ServerAliveInterval=15,ServerAliveCountMax=3 on-buildadm@hank.research.cs.dal.ca:/tank/data /data
