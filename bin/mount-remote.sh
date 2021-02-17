0dd1841fc5891e3526dd964d35fb2d856b445f90
Dwayne Hart <44408505+dwayne-hart@users.noreply.github.com>
2021-02-17 14:59:20 -0330

    Change the default Cipher algorithm
    Change the default Cipher to use the aes256-gcm@openssh.com algorithm. 
    
    on@dory:~$ cat test.sh
    for i in $(ssh -p 1022 -Q cipher) ; do dd if=/dev/zero bs=1000000 count=1000 2> /dev/null | ssh -p 1022 -c $i on@localhost "(time -p cat) > /dev/null" 2>&1 | grep real | awk '{print "'$i': "1000 / $2" MB/s" }'; done
    on@dory:~$ bash test.sh
    aes128-ctr: 381.679 MB/s
    aes192-ctr: 380.228 MB/s
    aes256-ctr: 404.858 MB/s
    aes128-gcm@openssh.com: 492.611 MB/s
    aes256-gcm@openssh.com: 520.833 MB/s
    chacha20-poly1305@openssh.com: 171.821 MB/s

--- bin/mount-remote.sh
+++ bin/mount-remote.sh
@@ -3,4 +3,4 @@
 ssh-keygen -R hank.research.cs.dal.ca -f ${HOME}/.ssh/known_hosts
 ssh-keyscan -4 -f ${HOME}/.ssh/known_hosts -t rsa hank.research.cs.dal.ca
 
-sshfs -o ro,reconnect,allow_other,ServerAliveInterval=15,ServerAliveCountMax=3 on-buildadm@hank.research.cs.dal.ca:/tank/data /data
+sshfs -o Ciphers=aes256-gcm@openssh.com,ro,reconnect,allow_other,ServerAliveInterval=15,ServerAliveCountMax=3 on-buildadm@hank.research.cs.dal.ca:/tank/data /data
