5f9ca97b9bf9ca11ff80dc7c410369ad8ff2a05d
Dwayne Hart <44408505+dwayne-hart@users.noreply.github.com>
2021-03-25 09:29:03 -0230

    Update mount-remote.sh
    - Changed the way the ssh-keyscan command to work by having the datastore key automatically postpended to the user's known_hosts file. Removed the behavior of asking the user if they wish to do so.
    - Changed the default cipher to be chacha20-poly1305@openssh.com as its one of the more faster algorithms.

--- bin/mount-remote.sh
+++ bin/mount-remote.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
 ssh-keygen -R hank.research.cs.dal.ca -f ${HOME}/.ssh/known_hosts
-ssh-keyscan -4 -f ${HOME}/.ssh/known_hosts -t rsa hank.research.cs.dal.ca
+ssh-keyscan -4 -t rsa hank.research.cs.dal.ca | tee -a ${HOME}/.ssh/known_hosts
 
-sshfs -o Ciphers=aes256-gcm@openssh.com,ro,reconnect,allow_other,ServerAliveInterval=15,ServerAliveCountMax=3 on-buildadm@hank.research.cs.dal.ca:/tank/data /data
+sshfs -o Cipher=chacha20-poly1305@openssh.com,ro,reconnect,allow_other,ServerAliveInterval=15,ServerAliveCountMax=3 on-buildadm@hank.research.cs.dal.ca:/tank/data /data
