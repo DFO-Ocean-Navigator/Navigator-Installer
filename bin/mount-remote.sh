a6fdc379522642b837ddea90aeb9cebd65107c0f
Dwayne Hart <dwayne.hart@gmail.com>
2021-08-26 15:56:46 -0230

    Update tag version for the Ocean-Navigator-Project and changed the mount-remote.sh script to test if the /data directory exists.

--- bin/mount-remote.sh
+++ bin/mount-remote.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 
-ssh-keygen -R hank.research.cs.dal.ca -f ${HOME}/.ssh/known_hosts
-ssh-keyscan -4 -t rsa hank.research.cs.dal.ca | tee -a ${HOME}/.ssh/known_hosts
+if [ ! -d /data ] ; then
+    echo "Please have an admin create the /data directory and that ownership be given to ubuntu."
+    exit 1
+fi
 
 sshfs -o Cipher=chacha20-poly1305@openssh.com,ro,reconnect,allow_other,ServerAliveInterval=15,ServerAliveCountMax=3 on-buildadm@hank.research.cs.dal.ca:/tank/data /data
